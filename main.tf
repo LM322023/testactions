3350feac4fc74e44993ca7dab5e47e14f9c488ee
tahmichael25 <tahmichael_5@yahoo.co.uk>
2023-09-19 16:57:32 -0400

    updates 2

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 1.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
 }
