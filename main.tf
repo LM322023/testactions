5e5054457042088731f39c3373c260d2635dbc87
tahmichael25 <tahmichael_5@yahoo.co.uk>
2023-09-19 17:23:45 -0400

    updates 0

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
-    }
-  }
-required_version = ">=0.14.9"
-}
-
 provider "aws" {
-  version = "~>3.0"
   region  = "us-west-2"
 }
 resource "aws_vpc" "test" {
