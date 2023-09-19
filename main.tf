0474c08da50592867373495eff35487040d6dba5
tahmichael25 <tahmichael_5@yahoo.co.uk>
2023-09-19 17:20:40 -0400

    updates 0

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+}
+
 provider "aws" {
-  region = "us-west-2"
+  version = "~>3.0"
+  region  = "us-west-2"
 }
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
